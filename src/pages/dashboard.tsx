d59ae273660129d10a2c02d3372068ff39fa5b8d
mihalyi <cmihalyi@gmail.com>
2026-02-06 22:56:41 -0600

    removed useEffect and console.log in dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,15 +3,9 @@ import Transactions from "@/pages/components/transactions"
 import Messages from "@/pages/components/messages"
 import BalanceHistory from "@/pages/components/balance-history"
 import Expenditures from "@/pages/components/expenditures"
-import { useEffect } from "react"
 
 const Dashboard = () => {
 
-  useEffect(() => {
-    console.log("Dashboard effect")
-  }, [])
-
-  console.log("Rendering Dashboard")
   return (
     <>
       <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight text-balance">
